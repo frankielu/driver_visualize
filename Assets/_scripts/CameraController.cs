d40aa4119ce4a2ad61d3fc06a4bd5c2fca82326f
frankielu <frankie_lu@live.com>
2014-05-13 13:03:22 -0700

    initial working build

---
+++ Assets/_scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public GameObject player;
+	public float perspectiveZoomSpeed = 0.4f;
+	public float orthoZoomSpeed = 0.4f;
+	public float panSpeed = 200;
+	private Vector3 offset;
+
+	void Update () 
+	{
+		// pan the camera
+		if (Input.touchCount == 1 && Input.GetTouch (0).phase == TouchPhase.Moved) 
+		{
+			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+			transform.RotateAround(player.transform.position, Vector3.up, touchDeltaPosition.x * panSpeed * Time.deltaTime);
+//			transform.RotateAround(player.transform.position, Vector3.right, touchDeltaPosition.y * panSpeed * Time.deltaTime);
+//			transform.LookAt(player.transform.position + new Vector3(0.0f, 1.0f, 0.0f));
+		}
+
+		// zoom the camera in and out
+		if (Input.touchCount == 2) 
+		{
+			Touch touch0 = Input.GetTouch(0);
+			Touch touch1 = Input.GetTouch(1);
+
+			Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+			Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+
+			float prevDeltaMag = (touch0PrevPos - touch1PrevPos).magnitude;
+			float touchDeltaMag = (touch0.position - touch1.position).magnitude;
+
+			float deltaMagDiff = prevDeltaMag - touchDeltaMag;
+
+			if (camera.isOrthoGraphic)
+			{
+				camera.orthographicSize += deltaMagDiff * orthoZoomSpeed;
+				camera.orthographicSize = Mathf.Max(camera.orthographicSize, 0.1f);
+			}
+			else
+			{
+				camera.fieldOfView += deltaMagDiff * perspectiveZoomSpeed;
+				camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, 15, 140);
+			}
+		}
+	}
+}
---
+++ Assets/_scripts/Player_Controller.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Linq;
+
+public class Player_Controller : MonoBehaviour 
+{
+	public GameObject bodypart_Head;
+	public GUIText errorDialog;
+	public TextAsset textDataFile;
+
+	private Vector3 originalPosition;
+	private Quaternion originalRotation;
+	private bool startAnimation = false;
+	private int count = 1;
+	private float[][] movementData = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+		errorDialog.text = "No errors.";
+
+		// load data file
+		try
+		{
+			string[] splitLines = textDataFile.text.Split(new string[] { "\n" }, StringSplitOptions.None);
+			string[][] linesDelimited = splitLines.Select(line => line.Split(',').ToArray()).ToArray();
+
+			movementData = linesDelimited.Select(line => line.Select(x => float.Parse(x)).ToArray()).ToArray();
+		}
+		catch
+		{
+			errorDialog.text = "Unable to read text file!";
+			return;
+		}
+
+		originalPosition = bodypart_Head.transform.position;
+		originalRotation = bodypart_Head.transform.rotation;
+
+//		bodypart_Head.transform.eulerAngles = new Vector3 (bodypart_Head.transform.eulerAngles.x, 
+//		                                                 bodypart_Head.transform.eulerAngles.y + movementData [0] [2],
+//		                                                 bodypart_Head.transform.eulerAngles.z);
+
+		// need to set initial condition for pitch and roll as well
+	}
+
+	void FixedUpdate () 
+	{
+		if (startAnimation == true) 
+		{
+			// these values need to be multiplied by a multiplier depending on how degrees are measured by the sensor
+			float yawMovement = -1.0f * (movementData [count] [2] - movementData [count - 1] [2]);
+			float pitchMovement = 1.0f * (movementData [count] [1] - movementData [count - 1] [1]);
+			float rollMovement = -1.0f * (movementData [count] [3] - movementData [count - 1] [3]);
+
+			// offset is currently (0.0, 0.4, 0.0)
+			bodypart_Head.transform.RotateAround (originalPosition, Vector3.up, yawMovement);
+			bodypart_Head.transform.RotateAround (new Vector3 (0.0f, 1.55f, 0.0f),
+                              new Vector3 (1.0f, 0.0f, 0.0f),
+                              pitchMovement * 0.6f);
+			bodypart_Head.transform.RotateAround (new Vector3 (0.0f, 1.45f, 0.0f),
+                             new Vector3 (0.0f, 0.0f, 1.0f),
+                             rollMovement * 0.5f);
+
+			count = count + 1;
+		}
+		else
+		{
+			bodypart_Head.transform.position = originalPosition;
+			bodypart_Head.transform.rotation = originalRotation;
+			count = 1;
+		}
+	}
+
+	void LateUpdate ()
+	{
+		// the back button of an android device quits the game
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+	void OnGUI()
+	{
+		if(GUI.Button(new Rect(20,60,80,50), "Reset")) 
+		{
+			if (startAnimation == true) startAnimation = false;
+			else startAnimation = true;
+		}
+	}
+
+}
